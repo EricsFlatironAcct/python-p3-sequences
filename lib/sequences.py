8cd53f31151614718c9d33f9367d739124246341
Eric Krukowski <erickruk@amazon.com>
2023-07-03 05:35:57 -0400

    Lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length==0:
+        print("[]")
+    else:
+        fib_list = []
+        fib_list.append(0)
+        if length > 1:
+            fib_list.append(1)
+            for n in range(2, length):
+                fib_list.append(fib_list[n-2]+fib_list[n-1])
+        print(fib_list)
